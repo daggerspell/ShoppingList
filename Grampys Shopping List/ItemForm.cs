30c4c65c0b03a4b2743d3d59bd8d07fe26e18870
James Blackman <jmsblckmn@gmail.com>
2020-05-25 14:18:05 -0500

    Added initial Item Form

---
+++ Grampys Shopping List/ItemForm.cs
@@ -0,0 +1,40 @@
+﻿using CommonLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grampys_Shopping_List
+{
+    public partial class ItemForm : Form
+    {
+        public string Mode = "Edit";
+        public Item currentItem = null;
+        public ItemForm()
+        {
+            InitializeComponent();
+        }
+
+        public ItemForm(string formMode)
+        {
+            Mode = formMode;
+            this.Text = Mode + " Item";
+            InitializeComponent();
+        }
+
+        public ItemForm(string formMode, Item inputItem)
+        {
+            Mode = formMode;
+            this.Text = Mode + " Item";
+            currentItem = inputItem;
+            InitializeComponent();
+
+            //TODO: after initialize load Item into fields
+        }
+    }
+}
